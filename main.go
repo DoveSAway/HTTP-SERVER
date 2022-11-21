a7fa081adc6a86a9d9945139c84b8e14feb8e91c
rafael lafon <rafaellafonrivegauche@gmail.com>
2022-11-21 21:18:09 +0100

    fix

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World !"))
+	})
+	http.ListenAndServe(":8080", nil)
 }
