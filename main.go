7f4f66a6643f0d574e2ff505135dd1b502917b3d
rafael lafon <rafaellafonrivegauche@gmail.com>
2022-11-22 12:38:47 +0100

    jnff

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World !"))
 	})
 	http.ListenAndServe(":8080", nil)
