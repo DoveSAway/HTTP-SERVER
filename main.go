d93d90e8f62aa417c1443d8622701b0a8e0e165b
rafael lafon <rafaellafonrivegauche@gmail.com>
2022-11-25 14:13:18 +0100

    MAJ

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
+type Page struct {
+	Valeur string
+}
+
+var templates = template.Must(template.ParseFiles("home.html"))
+
 func main() {
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World !"))
-	})
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/home", homeHandler)
+	http.ListenAndServe(":9999", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	p := Page{Valeur: "mon premier essai"}
+	err := templates.ExecuteTemplate(w, "home.html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
